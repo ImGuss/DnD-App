841e7b51c1e903b8e48146055c967d582eeab891
Gustavo Gutierrez <imguss@gmail.com>
2017-06-05 13:22:14 -0400

    added profile view

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const ensure = require('connect-ensure-login');
+
+const User = require('../models/user-model');
+const Character = require('../models/character-model');
 
 const userRoute = express.Router();
 
 
+userRoute.get(
+  '/profile',
+  ensure.ensureLoggedIn('login'),
+  (req, res, next) => {
+
+    Character.find(
+      {owner: req.user._id},
+      {firstName: 1},
+      (err, characterList) => {
+        if (err) {
+          next(err);
+          return;
+        }
+
+        res.render('users/profile-view', {characters: characterList});
+      }
+    );
+  }
+);
 
 
 
